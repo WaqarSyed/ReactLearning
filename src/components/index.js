e6e4eba7321c194a30bf2470335317f3c41ddf57
waqarAli <alisyedwaqar108@gmail.com>
2024-09-20 23:12:25 +0500

    working upto 'pages' for login & signup  components

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,7 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
 
-export { Header, Footer, Container, Logo, LogoutBtn, RTE };
+export { Header, Footer, Container, Logo, LogoutBtn, RTE, Signup, Login };
