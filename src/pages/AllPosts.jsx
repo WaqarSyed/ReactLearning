25c512a780c2f5d3595408358f498144c05827d7
waqarAli <alisyedwaqar108@gmail.com>
2024-09-25 01:03:47 +0500

    all posts are showing on home

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -10,16 +10,14 @@ function AllPosts() {
       }
     });
   }, []);
+
   return (
     <div className="w-full py-8">
       <Container>
-        {/* {posts.map(() => (
-                <PostCard key={post.$id} post={post} />
-            ))} */}
         <div className="flex flex-wrap">
           {posts.map((post) => (
             <div key={post.$id} className="p-2 w-1/4">
-              <PostCard post={post} />
+              <PostCard {...post} />
             </div>
           ))}
         </div>
