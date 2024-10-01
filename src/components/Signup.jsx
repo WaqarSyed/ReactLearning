f19409e777e7c16f0ee221a2bb46e265cffd5331
waqarAli <alisyedwaqar108@gmail.com>
2024-10-01 19:01:32 +0500

    recreated Signup component by understanding the key addOns of react-hook-form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -17,7 +17,7 @@ function Signup() {
     try {
       const userData = await authService.createAccount(data);
       if (userData) {
-        const userData = await authService.getCurrentUser();
+        const userData = await authService.getCurrentUser(userData);
         if (userData) dispatch(login(userData));
         navigate("/");
       }
@@ -25,6 +25,7 @@ function Signup() {
       setError(error.message);
     }
   };
+
   return (
     <div className="flex items-center justify-center">
       <div
