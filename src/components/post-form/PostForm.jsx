05d29c87d2efc48d370e9cc3a602ecc55a7985fb
waqarAli <alisyedwaqar108@gmail.com>
2024-10-07 17:16:44 +0500

    PostForm component recreated by fully understanding it

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,40 +1,37 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
+import { Button, Input, Select, RTE } from "../index";
 import { useForm } from "react-hook-form";
-import { Button, Input, Select, RTE } from "..";
 import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function PostForm({ post }) {
-  const { register, handleSubmit, watch, setValue, control, getValues } =
+  const { register, handleSubmit, control, setValue, getValues, watch } =
     useForm({
       defaultValues: {
         title: post?.title || "",
-        slug: post?.$id || "",
+        slug: post?.slug || "",
         content: post?.content || "",
-
         status: post?.status || "active",
       },
     });
   const navigate = useNavigate();
   const userData = useSelector((state) => state.auth.userData);
-  // console.log(userData);
 
   const submit = async (data) => {
     if (post) {
       const file = data.image[0]
         ? await appwriteService.uploadFile(data.image[0])
         : null;
-
       if (file) {
-        appwriteService.deleteFile(post.featuredImage);
+        appwriteService.deleteFile(post.$id);
       }
       const dbPost = await appwriteService.updatePost(post.$id, {
         ...data,
         featuredImage: file ? file.$id : undefined,
       });
       if (dbPost) {
-        navigate(`/post/${dbPost.$id}/`);
+        navigate(`/post/${dbPost.$id}`);
       }
     } else {
       const file = await appwriteService.uploadFile(data.image[0]);
@@ -52,24 +49,21 @@ export default function PostForm({ post }) {
     }
   };
   const slugTransform = useCallback((value) => {
-    if (value && typeof value === "string")
+    if (value && typeof value === "string") {
       return value
         .trim()
         .toLowerCase()
         .replace(/[^a-zA-Z\d\s]+/g, "-")
         .replace(/\s/g, "-");
-
-    return "";
+    }
   }, []);
-
-  React.useEffect(() => {
+  useEffect(() => {
     const subscription = watch((value, { name }) => {
       if (name === "title") {
         setValue("slug", slugTransform(value.title, { shouldValidate: true }));
       }
     });
     console.log(post);
-
     return () => subscription.unsubscribe();
   }, [watch, slugTransform, setValue]);
   return (
